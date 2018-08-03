f0bfcfaefeaef7f368eb47870b8f4d3c9bb5f1c7
Aaron Russell <russell.aaron94@gmail.com>
2018-08-03 13:39:21 -0600

    Validate font filenames before trying to generate @font-face rules

--- generate-font-faces.js
+++ generate-font-faces.js
@@ -2,25 +2,33 @@ const fs = require('fs');
 const process = require('process');
 
 const fontsDirectory = 'fonts';
+const outputFilename = 'font-faces.css';
 
 fs.readdir(fontsDirectory, (err, filenames) => {
   if (err) {
-    console.error(`Failed to read the contents of ${fontsDirectory}.`, err);
+    console.error(`Failed to read the contents of '${fontsDirectory}.'`, err);
     process.exit(1);
   }
 
-  // TODO: Validate filenames.
-
-  // Generate a @font-face for the filename.
-  const fontFaceRules = filenames.map(filename =>
+  const fontFaceRules = filenames
+    .filter(filename => {  // Ignore files that aren't TTFs.
+      const extension = filename.substring(filename.lastIndexOf('.') + 1);
+      if (!/ttf$/ig.test(extension)) {
+        console.warn(`'${filename}' is not a TTF file and will be ignored.`);
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .map(filename =>  // Generate a @font-face rule for each of the font files.
 `@font-face {
-  font-family: "${filename.slice(0, filename.length - 4)}";
+  font-family: "${filename.substring(filename.length - 4)}";
   src: url("fonts/${filename}");
 }`
-  );
+    );
 
   // Write each of the generated font faces to a CSS file.
-  const fileOutputStream = fs.createWriteStream('font-faces.css');
+  const fileOutputStream = fs.createWriteStream(outputFilename);
   fontFaceRules.forEach((fontFaceRule, index) => {
     fileOutputStream.write(fontFaceRule);
 
