9a9b6ba6fdd92043464cde52442aedbe6ea4851f
Aaron Russell <russell.aaron94@gmail.com>
2018-08-03 22:53:10 -0600

    Break out generation of CSS and JS into separate functions

--- generate-font-faces.js
+++ generate-font-faces.js
@@ -4,28 +4,14 @@ const process = require('process');
 const fontsDirectory = 'fonts';
 const outputFilename = 'font-faces.css';
 
-fs.readdir(fontsDirectory, (err, filenames) => {
-  if (err) {
-    console.error(`Failed to read the contents of '${fontsDirectory}.'`, err);
-    process.exit(1);
-  }
-
-  const fontFaceRules = filenames
-    .filter(filename => {  // Ignore files that aren't TTFs.
-      const extension = filename.substring(filename.lastIndexOf('.') + 1);
-      if (!/ttf$/ig.test(extension)) {
-        console.warn(`'${filename}' is not a TTF file and will be ignored.`);
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .map(filename =>  // Generate a @font-face rule for each of the font files.
+const generateCss = filenames => {
+  // Generate a @font-face rule for each of the font files.
+  const fontFaceRules = filenames.map(filename =>
 `@font-face {
-  font-family: "${filename.substring(filename.length - 4)}";
+  font-family: "${filename.substring(0, filename.length - 4)}";
   src: url("fonts/${filename}");
 }`
-    );
+  );
 
   // Write each of the generated font faces to a CSS file.
   const fileOutputStream = fs.createWriteStream(outputFilename);
@@ -41,4 +27,30 @@ fs.readdir(fontsDirectory, (err, filenames) => {
   });
 
   fileOutputStream.end();
+};
+
+const generateJs = filenames => {
+  // TODO: Implement something that creates a JS array of fontnames that can be imported into React components.
+  console.log(filenames);
+};
+
+fs.readdir(fontsDirectory, (err, filenames) => {
+  if (err) {
+    console.error(`Failed to read the contents of '${fontsDirectory}.'`, err);
+    process.exit(1);
+  }
+
+  // Ignore files that aren't TTFs.
+  const ttfFilenames = filenames.filter(filename => {
+    const extension = filename.substring(filename.lastIndexOf('.') + 1);
+    if (!/ttf$/ig.test(extension)) {
+      console.warn(`'${filename}' is not a TTF file and will be ignored.`);
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  generateCss(filenames);
+  generateJs(filenames);
 });
