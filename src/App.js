1f2efba1303d8ca5e9a188ff0615bb8823dc1ebc
Lanh Nguyen <LanhNguyen@u.boisestate.edu>
2018-10-09 19:21:22 -0600

    WIP - front end

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import fonts from './font-details.json';
+import button from './topButton.svg';
 
 const App = () => (
   <React.Fragment>
     <div className="header">
       <span>Boise State GDes Typography</span>
-      <a className="about" href="https://wwww.google.com/" onClick={( ) => console.log("CLICK")}>About</a>
+      <a className="about" href="/about" onClick={( ) => console.log("CLICK")}>About</a>
     </div>
     <div>
       {fonts.map((font, index) => (
         <div className="fontContainer" key={index}>
 
           <div className="fontFamily">{font.fontFamily}</div>
-          <div className="fontDetails">{font.author} -- {font.year} -- {font.course}</div>
+          <div className="fontDetails">{font.author}&nbsp; |&nbsp; {font.year}&nbsp; |&nbsp; {font.course}</div>
           <div className="grid-container">
             <div>
               <div className="fontPreview" style={{ fontFamily: font.fontFamily }}>
@@ -32,14 +33,19 @@ const App = () => (
             <span className="fontDescript">
               {font.description}
             </span>
-            <span>
-              {/* <button>View Specimen</button>
-              <button>Download</button> */}
+            <span className="buttonContainer">
+              <button className="buttonSpecimen">
+                <a method="get" href={`specimen/${font.fontFamily}.pdf`} download>View Specimen</a>
+              </button>
+              <button className="buttonDownload">
+                <a method="get" href={`fonts/${font.fontFamily}.ttf`} download>Download</a>
+              </button>
             </span>
           </div>
         </div>
       ))}
     </div>
+      <button className="backToTop" ><img src={button} alt="back to top"></img></button>
   </React.Fragment>
 );
 
